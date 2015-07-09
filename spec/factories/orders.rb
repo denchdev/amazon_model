2a33d58322800446122f9d07438beda6ed1283ee
denromani4 <denromani4@gmail.com>
2015-07-09 15:26:09 +0300

    Update orders.rb

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :order do
     total_price {Faker::Number.decimal 4}
     completed_date {Faker::Date.forward 23}
-    state "in progress"
+    state Order::states.shuffle.first
   end
 
 end
