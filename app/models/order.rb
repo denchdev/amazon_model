cd956f6ce2025f81b2297136ca88900787697d5c
denromani4 <denromani4@gmail.com>
2015-07-09 15:11:28 +0300

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ActiveRecord::Base
-  POSSIBLE_STATES = ['in progress','completed','shipped']
+  states = %w(in\ progres complited shipped)
 
   belongs_to :customer
   belongs_to :credit_card
@@ -7,41 +7,14 @@ class Order < ActiveRecord::Base
   belongs_to :billing_address, class_name: "Address"
   belongs_to :shipping_address, class_name: "Address"
 
-  validates_presence_of :total_price
-  validates_presence_of :completed_date, if: -> {self.state == POSSIBLE_STATES[1]}
-  validates :state, inclusion: { in: POSSIBLE_STATES }, presence: true
+  validates_presence_of :total_price, :state
+  validates_presence_of :completed_date, if: -> {self.state == states[1]}
+  validates :state, inclusion: { in: states, message: "%{value} is not a valid value" }
 
   after_initialize :set_default_state
   after_initialize :set_default_total_price
   before_save :calc_and_set_total_price
 
-  scope :in_progress, -> {where(state: POSSIBLE_STATES[0])}
+  scope :in_progress, -> {where(state: state[0])}
 
-  def add_to_order(book, quantity=1)
-  order_item = self.order_items.find_by(book: book)
-    if order_item
-      order_item.quantity += quantity
-      order_item.save
-    else 
-      self.order_items.create(price: book.price, quantity: quantity, book: book)
-    end
-    self.save
-  end
-
-  private
-  def calc_and_set_total_price
-    if self.order_items.any?
-      self.total_price = self.order_items.inject(0.0) {|sum, i| sum + i.price*i.quantity} 
-    else 
-      self.total_price = 0.0
-    end
-  end
-
-  def set_default_total_price
-    self.total_price ||= 0.0
-  end
-
-  def set_default_state
-    self.state ||= POSSIBLE_STATES[0]
-  end
-end
+  
