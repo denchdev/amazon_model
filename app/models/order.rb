ab0ed8f5648db2d2f423d1f32917b638db8aecfc
denromani4 <denromani4@gmail.com>
2015-07-09 15:13:12 +0300

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,9 +4,9 @@ class Order < ActiveRecord::Base
   belongs_to :customer
   belongs_to :credit_card
   has_many :order_items
-  belongs_to :billing_address, class_name: "Address"
-  belongs_to :shipping_address, class_name: "Address"
-
+  belongs_to :billing_address,  class_name: "Address", foreign_key: "billing_address_id"
+  belongs_to :shipping_address, class_name: "Address", foreign_key: "shipping_address_id"
+  
   validates_presence_of :total_price, :state
   validates_presence_of :completed_date, if: -> {self.state == states[1]}
   validates :state, inclusion: { in: states, message: "%{value} is not a valid value" }
